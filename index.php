b4576964c4a59a708be3358c22a3efe437e7ddd7
oscarssanchez <osanpk@comunidad.unam.mx>
2017-12-15 16:54:14 -0600

    Added 1st CSS version. Updated the index.php file with the new CSS rules introduced in the first version of main.css

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ if ( have_posts() ) :
 	while ( have_posts() ) :
 		the_post();
 		?>
-		<div class="col-md-3">
-			<h5><?php esc_html( get_the_date() ); ?></h5>
+		<div class="col-md-3 post">
+			<h5 class="post-date uppercase separation"><?php esc_html( get_the_date() ); ?></h5>
 			<a href="<?php the_permalink(); ?>"><h3><?php esc_html( get_the_title() ); ?></h3></a>
 		</div>
 	<?php endwhile; else : ?>
