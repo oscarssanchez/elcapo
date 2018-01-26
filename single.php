1a24d8619e63477ce444d9ecb7e8f5bf1434f453
Oscar Sanchez <osanpk@comunidad.unam.mx>
2018-01-26 13:12:22 -0500

    Formatting changes in single.php

--- single.php
+++ single.php
@@ -1,14 +1,11 @@
 <?php
-	get_header(); ?>
-<?php
+get_header();
 while ( have_posts() ) :
 	the_post();
 	get_template_part( 'template-parts/content', get_post_type() );
-	if ( comments_open() || get_comments_number() ) :
+	if ( comments_open() && ( 0 !== get_comments_number() ) ) :
 		comments_template();
 	endif;
 endwhile;
-	?>
-<?php
-	get_template_part( 'template-parts/contact', 'all' );
-	get_footer();
+get_template_part( 'template-parts/contact', 'all' );
+get_footer();
